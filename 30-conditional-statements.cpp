49ce805ae16262d222ea9684a6a71164169b2d64
The Lost Jedi <32588696+robocyber@users.noreply.github.com>
2018-07-23 19:06:19 +0530

    Create 30-conditional-statements.cpp

---
+++ 30-conditional-statements.cpp
@@ -0,0 +1,26 @@
+challenge- https://www.hackerrank.com/challenges/30-conditional-statements/problem
+very very easy
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    if(!(n%2==0))
+    {
+        cout<<"Weird";
+    }
+    else if (n>=2&&n<=5)
+        cout<<"Not Weird";
+    else if(n>=6&&n<=20)
+        cout<<"Weird";
+    else cout<<"Not Weird";
+
+    return 0;
+}
