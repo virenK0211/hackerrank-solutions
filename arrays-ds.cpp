39777e036fd7c08d27cf43f6bbee06c254dcadb2
The Lost Jedi <32588696+robocyber@users.noreply.github.com>
2018-09-02 10:48:40 +0530

    Create arrays-ds.cpp

---
+++ arrays-ds.cpp
@@ -0,0 +1,21 @@
+problem very easy  https://www.hackerrank.com/challenges/arrays-ds/problem
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{  int n;
+ cin>>n;
+ int ar[n];
+ for(int i=0;i<n;i++)
+ {
+     cin>>ar[i];
+ }
+ for(int i=n-1;i>=0;i--)
+ {
+     cout<<ar[i]<<" ";
+ }
+    
+    
+    return 0;
+}
